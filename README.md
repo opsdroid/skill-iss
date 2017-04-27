82a22763e3bb68eea7bef5232e619a0d39f0181b
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2017-04-27 15:59:12 +0100

    New skill ISS

--- README.md
+++ README.md
@@ -1,33 +1,40 @@
-# opsdroid skill hello
+# opsdroid skill ISS
 
-A skill for [opsdroid](https://github.com/opsdroid/opsdroid) to respond to hello and goodbye messages.
+A skill for [opsdroid](https://github.com/opsdroid/opsdroid) to locate and show a map of the ISS.
+
+Uses data from [open notify][open-notify] and [Google Maps][google-maps-api] for mapping.
 
 ## Requirements
 
-None.
+A Google Maps [API key][google-maps-api].
 
 ## Configuration
 
-None.
+```yaml
+skills:
+  - name: iss
+    # Required
+    api-key: "mygooglemapsapikey"
+    # Optional
+    zoom: "5"
+    map-size: "1024x768"
+    map-type: "hybrid" # hybrid, satellite or roadmap
+```
 
 ## Usage
 
-#### `hello`
+#### `Where is the ISS?`
 
-Says hello to the user.
+Shows the user a map of the ISS location
 
-> user: hello
+> user: Where is the ISS?
 >
-> opsdroid: Hi user
-
-#### `goodbye`
-
-Says goodbye to the user.
-
-> user: bye
->
-> opsdroid: Bye user
+> opsdroid:
+> ![ISS](https://cloud.githubusercontent.com/assets/1610850/25489364/7e09dffa-2b61-11e7-9539-788fa67544b2.png)
 
 ## License
 
 GNU General Public License Version 3 (GPLv3)
+
+[google-maps-api]: https://developers.google.com/maps/documentation/static-maps/intro
+[open-notify]: http://api.open-notify.org/
--- __init__.py
+++ __init__.py
@@ -1,16 +1,42 @@
+import aiohttp
+
 from opsdroid.matchers import match_regex
-import logging
-import random
 
-def setup(opsdroid):
-    logging.debug("Loaded hello module")
+ISS_API           = "http://api.open-notify.org/iss-now.json"
+GOOGLE_MAPS_API   = "https://maps.googleapis.com/maps/api/staticmap"
+DEFAULT_MAP_SIZE  = "1024x768"
+DEFAULT_ZOOM      = 5
+DEFAULT_MAP_TYPE  = "roadmap"
+GOOGLE_MAPS_QUERY = "{api}?center={lat},{lon}&zoom={zoom}&size={size}&maptype={type}&markers=color:blue%7Clabel:ISS%7C{lat},{lon}&key={api_key}"
 
-@match_regex(r'hi|hello|hey|hallo')
-async def hello(opsdroid, config, message):
-    text = random.choice(["Hi {}", "Hello {}", "Hey {}"]).format(message.user)
-    await message.respond(text)
 
-@match_regex(r'bye( bye)?|see y(a|ou)|au revoir|gtg|I(\')?m off')
-async def goodbye(opsdroid, config, message):
-    text = random.choice(["Bye {}", "See you {}", "Au revoir {}"]).format(message.user)
-    await message.respond(text)
+async def get_iss_location():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(ISS_API) as resp:
+            if resp.status == 200:
+                response = await resp.json()
+                return (response["iss_position"]["latitude"],
+                        response["iss_position"]["longitude"])
+
+
+@match_regex(r'[Ww]here( i|\'|)s the (iss|ISS|Iss|space station|international space station)')
+async def where_is_the_iss(opsdroid, config, message):
+    api_key  = config.get("api-key")
+    zoom     = str(config.get("zoom", DEFAULT_ZOOM))
+    map_size = config.get("map-size", DEFAULT_MAP_SIZE)
+    map_type = config.get("map-type", DEFAULT_MAP_TYPE)
+    lat, lon = await get_iss_location()
+
+    if api_key is None:
+        await message.respond(
+            "Whoops you need to configure this skill with a Google Maps API key.")
+        return
+
+    await message.respond(GOOGLE_MAPS_QUERY.format(api=GOOGLE_MAPS_API,
+                                                   lat=lat,
+                                                   lon=lon,
+                                                   zoom=zoom,
+                                                   size=map_size,
+                                                   type=map_type,
+                                                   api_key=api_key
+                                                   ))
